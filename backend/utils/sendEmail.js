d1d035994db17536c2e8b0933e1d6afb918b3a14
Dhan Raj <dhanrajkanu18@gmail.com>
2026-06-26 13:13:00 +0545

    Add NodeMailer timeouts

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,7 +6,10 @@ const sendEmail = async ({ to, subject, html }) => {
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
   });
 
   const mailOptions = {
